598199beb14348fe08513a082992beb2a4ffb4ef
Viacheslav Volodko <killobatt@gmail.com>
2024-04-27 23:34:43 +0300

    Proper spinner

--- main/main.c
+++ main/main.c
@@ -304,27 +304,14 @@ void create_demo_ui() {
     lv_style_set_text_color(&style_screen, lv_color_hex(0xFFFFFF));
     lv_obj_add_style(lv_scr_act(), &style_screen, LV_STATE_DEFAULT);
 
-        /*Create an Arc*/
-    lv_obj_t * arc = lv_arc_create(lv_screen_active());
-    lv_arc_set_rotation(arc, 270);
-    lv_arc_set_bg_angles(arc, 0, 360);
-    lv_obj_remove_style(arc, NULL, LV_PART_KNOB);   /*Be sure the knob is not displayed*/
-    lv_obj_remove_flag(arc, LV_OBJ_FLAG_CLICKABLE);  /*To not allow adjusting by click*/
-    lv_obj_center(arc);
+    lv_obj_t * spinner = lv_spinner_create(lv_screen_active());
+    lv_obj_set_size(spinner, 100, 100);
+    lv_obj_center(spinner);
+    lv_spinner_set_anim_params(spinner, 2000, 200);
 
     lv_obj_t *label = lv_label_create(scr);
     lv_label_set_text(label, "Scanning WiFi...");
     lv_obj_align(label, LV_ALIGN_CENTER, 0, 100);
-
-    lv_anim_t a;
-    lv_anim_init(&a);
-    lv_anim_set_var(&a, arc);
-    lv_anim_set_exec_cb(&a, set_angle);
-    lv_anim_set_duration(&a, 1000);
-    lv_anim_set_repeat_count(&a, LV_ANIM_REPEAT_INFINITE);    /*Just for the demo*/
-    lv_anim_set_repeat_delay(&a, 500);
-    lv_anim_set_values(&a, 0, 100);
-    lv_anim_start(&a);
 }
 
 static void lvgl_ui_task(void *params) {
@@ -425,7 +412,7 @@ esp_err_t scan_wifi_networks(uint16_t *number, wifi_ap_record_t *ap_records) {
 
 void wifi_scan_task(void *params) {
     // Emulating process until I find out why ESP32S3 does not go beyond ESP_WIFI_START();
-    vTaskDelay(pdMS_TO_TICKS(4000));
+    vTaskDelay(pdMS_TO_TICKS(6000));
 
     lv_async_call(create_wifi_ap_list_screen, NULL);
 
